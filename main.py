d1d70cbe299939ee318d4a21fca0d85dfcc43fab
AlTaberOwO <Antonnikov07@bk.ru>
2022-04-09 19:30:52 +0200

    МоЖнА СоЗдАвАтЬ ПрИлОжЕнИя ПоЛьЗоВаТеЛяМ

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for
+from flask import redirect
+
+from .models import App
 from . import db
 from flask_login import current_user, login_required
 
@@ -14,3 +17,29 @@ def index():
 @main.route('/profile')
 def profile():
     return render_template('profile.html')
+
+
+@login_required
+@main.route('/createapp', methods=['POST'])
+def create_post_app():
+    app = App()
+
+    app.name = request.form.get('name')
+    app.description = request.form.get('description')
+    app.publisher = current_user.id
+    app.version = request.form.get('version')
+    app.tags = request.form.get('tags')
+    app.screenshots = request.form.get('screenshots')
+    app.big_icon = request.form.get('big_icon')
+    app.small_icon = request.form.get('small_icon')
+    app.is_published = False
+    db.session.add(app)
+    db.session.commit()
+
+    return redirect(url_for('main.index'))
+
+
+@login_required
+@main.route('/createapp')
+def create_app():
+    return render_template('createapp.html')
